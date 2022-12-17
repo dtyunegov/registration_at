8c9fb5dfa970970fa003cb83a4ee6af772992b56
dtyunegov <dtyunegov@gmail.com>
2022-12-17 11:53:03 +0300

    add email tests

--- project/tests/validation.test.ts
+++ project/tests/validation.test.ts
@@ -15,27 +15,41 @@ test.describe('Registration form', async () => {
         expect.soft(await page.textContent(registrationPage.selectors.confirmPassword + registrationPage.selectors.error)).toBe(messages.confipassword.isRequired)
     })
 
-    new Array(
+    Array(
         "Log",
         "LOGIN",
         "login",
         "LoGin",
         "login-login",
         "login_login",
-        "login login"
+        "login login",
+        "login123"
     ).forEach(value => {
         test(`Login is correct. Value: ${value}`, async ({ page, registrationPage, alert }) => {
-            await page.fill(registrationPage.selectors.login, value)
-            await page.fill(registrationPage.selectors.email, "email@test.com")
-            await page.fill(registrationPage.selectors.password, "Password123$")
-            await page.fill(registrationPage.selectors.confirmPassword, "Password123$")
-            await page.click(registrationPage.selectors.signUpButton);
-            expect(await (await page.waitForSelector(alert.selectors.success)).isVisible()).toBeTruthy()
+            await registrationIsSuccess(page, alert, registrationPage, value, "test@mail.ru", "Password123")
         })
     })
 
-    test(`Login is incorrect.`, async ({ page, registrationPage, alert }) => {
+    test(`Login is incorrect.`, async ({ page, registrationPage }) => {
         await page.fill(registrationPage.selectors.login, "!@#")
-        expect(await (await page.waitForSelector(alert.selectors.success)).isVisible()).toBeTruthy()
+        expect(await page.textContent(registrationPage.selectors.login + registrationPage.selectors.error)).toBe(messages.login.incorrect)
+    })
+
+    Array(
+        "qwerty@mail.ru",
+        "qwe.qwe@mail.ru"
+    ).forEach(email => {
+        test(`Email is correct. element: ${email}`, async ({ page, registrationPage, alert }) =>
+            await registrationIsSuccess(page, alert, registrationPage, "Login", email, "Password123")
+        )
     })
 })
+
+async function registrationIsSuccess(page, alert, registrationPage, login, email, password) {
+    await page.fill(registrationPage.selectors.login, login)
+    await page.fill(registrationPage.selectors.email, email)
+    await page.fill(registrationPage.selectors.password, password)
+    await page.fill(registrationPage.selectors.confirmPassword, password)
+    await page.click(registrationPage.selectors.signUpButton);
+    expect(await (await page.waitForSelector(alert.selectors.success)).isVisible()).toBeTruthy()
+}
